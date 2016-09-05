960fff506a4d139ff43b6897e52b377e35ba1021
Dmitry Overchenko <Dmitry Overchenko>
2016-09-05 23:42:18 +0300

    auth

--- lightit_test/urls.py
+++ lightit_test/urls.py
@@ -20,7 +20,7 @@ import main.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^login/', login.views.login, name='login'),
+    url(r'^login/$', login.views.login, name='login'),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url('', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^', include('main.urls'))
--- login/views.py
+++ login/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def login(request):
+    if not request.user.is_anonymous():
+        print request.user.get_full_name()
+        return redirect('main')
     context = {'title': 'Login',
                'user': request.user}
     return render(request, 'login.html', context)
